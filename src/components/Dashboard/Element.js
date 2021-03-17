9137987e1755fc29b2449aaaeb60b4ecba307bd2
Brian Aditya <brianadit33@gmail.com>
2021-03-17 23:37:37 +0800

    dashborad home 70%

--- src/components/Dashboard/Element.js
+++ src/components/Dashboard/Element.js
@@ -17,12 +17,37 @@ export const DashContainer = styled.div`
 export const DashHeading = styled.div`
    width: 100%;
    height: 45vh;
+   border-radius: 15px;
+   display: flex;
+   justify-content: space-between;
+`
+export const Update = styled.div`
+   width: 350px;
+   height: 100%;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   flex-direction: column;
    background-color: var(--primary);
    border-radius: 15px;
 `
+export const UpdateDesc = styled.h1`
+   font-family: var(--inter);
+   color: #fff;
+`
+
+export const HeadingUser = styled.div`
+   width: 65%;
+   height: 65%;
+   display: flex;
+`
+
+export const UserDesc = styled.h1`
+   font-family: var(--inter);
+`
 
 export const DashArticle = styled.div`
-   width: 90%;
+   width: 100%;
    height: 40vh;
    background-color: var(--primary);
    border-radius: 15px;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,5 @@
-import * as React from "react"
+import React, { useState } from "react"
+import ReactCardFlip from 'react-card-flip';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,23 +10,52 @@ import {
    DashContainer,
    DashHeading,
    DashWrapper,
+   Update,
+   HeadingUser,
+   UpdateDesc,
+   UserDesc,
 } from '../components/Dashboard/Element'
 
-const Dashboard = () => (
-   <Layout>
-      <SEO title="dashboard" />
-      <DashWrapper>
-         <SideNav />
-         <DashContainer>
-            <DashHeading>
 
-            </DashHeading>
-            <DashArticle>
+const Dashboard = () => {
+   const [isFlipped, setIsFlipped] = useState(false);
 
-            </DashArticle>
-         </DashContainer>
-      </DashWrapper>
-   </Layout>
-)
+   const handleClick = () => {
+      setIsFlipped(!isFlipped);
+   };
+
+   return (
+      <Layout>
+         <SEO title="dashboard" />
+         <DashWrapper>
+            <SideNav />
+            <DashContainer>
+               <DashHeading>
+                  <HeadingUser>
+                     <UserDesc>Hello, User!</UserDesc>
+                  </HeadingUser>
+                  <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+                     <Update>
+                        <UpdateDesc>Whats New?</UpdateDesc>
+                        <button onClick={handleClick}>Click to flip</button>
+                     </Update>
+
+                     <Update>
+                        This is the back of the card.
+                        <button onClick={handleClick}>Click to flip</button>
+                     </Update>
+                  </ReactCardFlip>
+                  {/* <Update>
+                  <UpdateDesc> Whats New? </UpdateDesc>
+               </Update> */}
+               </DashHeading>
+               <DashArticle>
+
+               </DashArticle>
+            </DashContainer>
+         </DashWrapper>
+      </Layout>
+   );
+}
 
 export default Dashboard
